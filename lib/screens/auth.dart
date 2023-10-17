9e7dbc79d970b698a94a44d5b4956c51f6438bf2
MSKMeta <mohammed.alkadi.alsaleh98@gmail.com>
2023-10-17 15:43:29 +0300

    Signin page refinemnet

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -18,8 +18,12 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
   var _isLogin = true;
+  var _isPatient = true;
+  var _isDoctor = false;
   var _enteredEmail = '';
   var _enteredPassword = '';
+  bool _inValidEmail = false;
+  bool _inValidPassword = false;
 
   void _submit() async {
     final isValid = _formKey.currentState!.validate();
@@ -79,35 +83,130 @@ class _AuthScreenState extends State<AuthScreen> {
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                          margin: const EdgeInsets.fromLTRB(0, 16, 200, 16),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 2, horizontal: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              // style: ElevatedButton.styleFrom(
+                              //   shape: RoundedRectangleBorder(
+                              //     borderRadius: BorderRadius.circular(8),
+                              //   ),
+
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  backgroundColor: _isPatient
+                                      ? Theme.of(context).colorScheme.primary
+                                      : Theme.of(context).colorScheme.secondary,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isPatient = true;
+                                    _isDoctor = false;
+                                  });
+                                },
+                                child: Text(
+                                  'Patient',
+                                  style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary),
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                  shadowColor:
+                                      Theme.of(context).colorScheme.secondary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  backgroundColor: _isDoctor
+                                      ? Theme.of(context).colorScheme.primary
+                                      : Theme.of(context).colorScheme.secondary,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isPatient = false;
+                                    _isDoctor = true;
+                                  });
+                                },
+                                child: Text(
+                                  'Doctor',
+                                  style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                         TextFormField(
                           decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
+                            focusedErrorBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(8),
                               borderSide: const BorderSide(
                                 style: BorderStyle.none,
                               ),
                             ),
-                            filled: true,
-                            fillColor: Theme.of(context).colorScheme.secondary,
-                            labelText: 'Email Adress',
-                            prefixIcon: const Icon(Icons.email_outlined),
-                            border: OutlineInputBorder(
+                            enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(8),
-                              borderSide: BorderSide(
-                                width: 0,
-                                color: Theme.of(context).colorScheme.secondary,
+                              borderSide: const BorderSide(
+                                style: BorderStyle.none,
                               ),
                             ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .secondaryContainer,
+                              ),
+                            ),
+                            border: const OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Theme.of(context).colorScheme.secondary,
+                            label: const Text('Email Address'),
+                            floatingLabelStyle: _inValidEmail
+                                ? TextStyle(
+                                    color: Theme.of(context).colorScheme.error,
+                                  )
+                                : TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .secondaryContainer,
+                                  ),
+                          ),
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
                           ),
                           keyboardType: TextInputType.emailAddress,
                           autocorrect: false,
                           textCapitalization: TextCapitalization.none,
                           validator: (value) {
-                            if (value == null ||
+                            final vemail = value == null ||
                                 value.trim().isEmpty ||
-                                !value.contains('@')) {
+                                !value.contains('@');
+                            if (vemail) {
+                              setState(() {
+                                _inValidEmail = vemail;
+                              });
                               return 'please enter valid email';
                             }
+                            setState(() {
+                              _inValidEmail = vemail;
+                            });
                             return null;
                           },
                           onSaved: (value) {
@@ -119,17 +218,56 @@ class _AuthScreenState extends State<AuthScreen> {
                         ),
                         TextFormField(
                           decoration: InputDecoration(
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: const BorderSide(
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: const BorderSide(
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .secondaryContainer,
+                              ),
+                            ),
+                            border: const OutlineInputBorder(),
                             filled: true,
                             fillColor: Theme.of(context).colorScheme.secondary,
-                            labelText: 'Password',
-                            prefixIcon: const Icon(Icons.key),
-                            border: const OutlineInputBorder(),
+                            label: const Text('Password'),
+                            floatingLabelStyle: _inValidPassword
+                                ? TextStyle(
+                                    color: Theme.of(context).colorScheme.error,
+                                  )
+                                : TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .secondaryContainer,
+                                  ),
+                          ),
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
                           ),
                           obscureText: true,
                           validator: (value) {
-                            if (value == null || value.trim().length < 6) {
+                            final vpassword =
+                                value == null || value.trim().length < 6;
+                            if (vpassword) {
+                              setState(() {
+                                _inValidPassword = vpassword;
+                              });
                               return 'password must be at least 6 characters long';
                             }
+                            setState(() {
+                              _inValidPassword = vpassword;
+                            });
                             return null;
                           },
                           onSaved: (value) {
@@ -139,26 +277,54 @@ class _AuthScreenState extends State<AuthScreen> {
                         const SizedBox(
                           height: 12,
                         ),
-                        ElevatedButton(
-                          onPressed: _submit,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                Theme.of(context).colorScheme.background,
-                          ),
-                          child: Text(
-                            _isLogin ? 'login' : 'signup',
-                            style: Theme.of(context).textTheme.titleLarge!,
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: _submit,
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.primary),
+                            child: Text(
+                              _isLogin ? 'Login' : 'signup',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimary),
+                            ),
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _isLogin = !_isLogin;
-                            });
-                          },
-                          child: Text(_isLogin
-                              ? 'create an account'
-                              : 'i already have an account. login'),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(
+                                _isLogin
+                                    ? 'create an account'
+                                    : 'already have an account',
+                                style: TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onPrimary),
+                              ),
+                            ),
+                            Text(
+                              'Forget your passowrd?',
+                              style: TextStyle(
+                                  color: Theme.of(context).colorScheme.error),
+                            ),
+                          ],
                         ),
                       ],
                     ),
