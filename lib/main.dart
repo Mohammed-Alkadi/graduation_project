e5b1c3b0c5fc33ddabaa4118bec24dee416c87d6
Abdullah Mohammed <abdullah.alhokbani@gmail.com>
2023-10-18 18:29:06 +0300

    Co-authored-by: Nawaf Alfntoukh <nnawaf103@Gmail.com> Co-authored-by: Mohammed-Alkadi <Mohammed-Alkadi@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/auth.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gp/screens/Auth/auth.dart';
 
 void main() {
   runApp(const App());
